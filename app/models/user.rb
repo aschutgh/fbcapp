39a246ce6c639729734cc26deecdd81fa95f4200
Arthur Schut <aschutgh@gmail.com>
2018-03-04 23:12:37 +0100

    added Friendship model and friend status to users index

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :messages
+  has_many :friendships, foreign_key: "me_id"
+  has_many :friends, through: :friendships
 end
