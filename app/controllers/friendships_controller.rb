d03eef865d6e6a3204b9395ee76707428ff2f21c
Arthur Schut <aschutgh@gmail.com>
2018-03-05 14:25:00 +0100

    added manage friends functionality

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,22 @@
 class FriendshipsController < ApplicationController
+
+
+  def index
+    @users = User.all
+  end
+
+
+  def create
+    user = User.find(params[:friend_id])
+    current_user.befriend(user)
+    user.befriend(current_user)
+    redirect_to user_friendships_path(current_user)
+  end
+
+  def destroy
+    user = Friendship.find(params[:id]).friend
+    current_user.defriend(user)
+    user.defriend(current_user)
+    redirect_to user_friendships_path(current_user)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :messages
-  has_many :friendships, foreign_key: "me_id"
+  has_many :friendships, foreign_key: "me_id", dependent: :destroy
   has_many :friends, through: :friendships
+
+
+  def befriend(other_user)
+    friends << other_user
+  end
+
+  def defriend(other_user)
+    friends.delete(other_user)
+  end
+
 end
