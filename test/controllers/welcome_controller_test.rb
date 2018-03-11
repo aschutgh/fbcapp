ab44252adc470a887b59734646aa76d2b58ef440
Arthur Schut <aschutgh@gmail.com>
2018-03-11 21:07:29 +0100

    first controller test get welcome index

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+
+
+  test "should get welcome index" do
     get welcome_index_url
     assert_response :success
+    assert_select "title", "Fbcapp"
   end
 
 end
